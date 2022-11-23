c82f39e1cd44557e991107e330d51956f05bf42b
Act0r1 <gizatullininsaf25@gmail.com>
2022-11-23 12:46:47 +0300

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message:":"Hello world"}
+
+
+
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
